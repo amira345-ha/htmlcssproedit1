78d1583b0c8d45afdb1e7ea4e7559dfa41349c8e
amira345-ha <ebrahimamira584@gmail.com>
2024-09-19 09:10:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var popup=document.getElementById["subow"];
+function opensub(){
+    popup.style.visibility="visible";
+}
+function closesub(){
+    popup.style.visibility="hidden";
+}
+function myfunction(){
+    var x = event.keycode;
+    console.log(x);
+    if(x==1)
+    (
+        closesub()
+    
+    )
+    var t=setTimeout(opensub,10000);
+}
+    
+   
+        
+    
